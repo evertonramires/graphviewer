f90bda508cf069f1af7df56d9159c630cd2840e9
Everton <ramiresero@gmail.com>
2025-04-27 14:59:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: Received `false` for a non-boolean attribute `active`.
    >
    > If you want to write it to the DOM, pass a string instead: active="false" or active={value.toString()}.
    >
    > If you used to conditionally omit it with active={condition && value}, pass active={condition ? value : undefined} instead.. Error source: src/components/ui/button.tsx (46:7) @ _c
    >
    >   44 |     const Comp = asChild ? Slot : "button"
    >   45 |     return (
    > > 46 |       <Comp
    >      |       ^
    >   47 |         className={cn(buttonVariants({ variant, size, className }))}
    >   48 |         ref={ref}
    >   49 |         {...props}
    >
    > Call Stack
    > 20
    >
    > Show 17 ignore-listed frame(s)
    > button
    > <anonymous> (0:0)
    > _c
    > src/components/ui/button.tsx (46:7)
    > Home
    > src/app/page.tsx (493:9)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -37,10 +37,11 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  active?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, active, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
